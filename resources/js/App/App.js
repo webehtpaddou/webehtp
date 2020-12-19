3a9a12087ef6dd0f86e9d21fc5ea0080ce53d599
Fikri Zakaria <fikrizakaria@outlook.com>
2020-12-19 17:50:24 +0100

    front integrated with back

---
+++ resources/js/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from "./components/Navbar/Navbar"
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Accueil from './Pages/Accueil'
+import Connexion from './Pages/Connexion'
+import Inscription from './Pages/Inscription'
+function App() {
+  return (
+      <Router>
+        <div>
+          <Navbar/>
+          <Switch>
+            <Route path='/' exact component={Accueil} />
+            <Route path='/connexion' component={Connexion} />
+            <Route path='/inscription' component={Inscription} />
+          </Switch>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
+
+if(document.getElementById('root')){
+  ReactDOM.render(<App/>,document.getElementById('root'))
+}
---
+++ resources/js/App/Pages/Connexion.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './connexion.css'
+
+class Connexion extends Component {
+  render() {
+    return (
+      <div className='connexion'>
+        <form>
+            <h1>Connexion</h1>
+            <div className='signin-dialog'>
+                <label htmlFor='email'>Email</label>
+                <input name='email' type='text'></input>
+                <label htmlFor='password'>Mot de passe</label>
+                <input name='password' type='password'></input>
+            </div>
+            <button type='submit'>Se connecter</button>
+            <a href="/forget" className="forget">Mot de pase oublié?</a>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Connexion
