345d137de598be7331df64a90f721a6d924e77bb
ipkminu <ipkminu@gmail.com>
2020-12-20 00:05:18 +0100

    pré-intégrationReact

---
+++ database/migrations/2020_12_16_004236_create_utilisateurs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilisateursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilisateurs', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('password');
+            $table->unsignedBigInteger('type_utilisateur'); //reférences types_users.id
+            $table->foreign('type_utilisateur')->references('id')->on('types_users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilisateurs');
+    }
+}
---
+++ database/migrations/2020_12_16_004320_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('telephone');
+            $table->string('ville');
+            $table->string('adresse');
+            $table->unsignedBigInteger('id_user'); //reférence utilisateurs.id
+            $table->foreign('id_user')->references('id')->on('utilisateurs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
