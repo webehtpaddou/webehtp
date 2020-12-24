1babb597695b89af95d740ee4e8d5d99e0133177
ipkminu <ipkminu@gmail.com>
2020-12-24 01:31:36 +0100

    conflits de branches résolu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //Creatin each type of seeder
         (new type_utilisateurSeeder)->run();
     }
 }
