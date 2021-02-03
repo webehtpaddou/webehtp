200b4b646be4461f7992b661cb073e6cff9701c8
Fikri Zakaria <fikrizakaria@outlook.com>
2021-02-03 04:11:12 +0100

    accueil fixe

---
+++ resources/js/App/Pages/Accueil.js
@@ -0,0 +1,224 @@
+import React, { Component } from 'react'
+class Accueil extends Component {
+  constructor(props){
+    super(props)
+    this.state={loading:true,
+      slider:[
+        {
+          img:"https://imgupload.io/images/2020/12/29/slider-1.jpg",
+          text:""
+        },
+        {
+          img:"https://imgupload.io/images/2020/12/29/slider-3.jpg",
+          text:""
+        },
+        {
+          img:"https://imgupload.io/images/2020/12/29/slider-2.jpg",
+          text:""
+        }],
+      best_sales:[
+        {
+          id:0,
+          name:"",
+          img:"https://imgupload.io/images/2020/12/29/product-1.jpg",
+          price:100
+        },
+        {
+          id:1,
+          name:"",
+          img:"https://imgupload.io/images/2020/12/29/product-2.jpg",
+          price:100
+        },
+        {
+          id:2,
+          name:"",
+          img:"https://imgupload.io/images/2020/12/29/product-3.jpg",
+          price:100
+        },
+        {
+          id:3,
+          name:"",
+          img:"https://imgupload.io/images/2020/12/29/product-4.jpg",
+          price:100
+        },
+        {
+          id:4,
+          name:"",
+          img:"https://imgupload.io/images/2020/12/29/product-5.jpg",
+          price:100
+        },
+      ]
+    }
+  }
+  async componentDidMount(){
+    const url="/products"
+    const response=await fetch(url)
+    const data=await response.json()
+    this.setState({items:data,loading:false})
+  }
+  closeProduct=()=>{
+    this.setState({showWindowProduct:false})
+  }
+  openProduct=()=>{
+    this.setState({showWindowProduct:true})
+  }
+  render() {
+    return (
+        <div className='accueil'>
+        <div className="header">
+            <div className="container-fluid">
+                <div className="row">
+                    
+                    <div className="col-md-8">
+                        <div className="header-slider normal-slider">
+                          {this.state.slider.map((elt,i)=>{
+                              return(
+                                <div key={i} className="header-slider-item">
+                                <img src={elt.img} alt="Slider Image" />
+                                <div className="header-slider-caption">
+                                    <p>{elt.text}</p>
+                                    <a className="btn" href=""><i className="fa fa-shopping-cart"></i>Shop Now</a>
+                                </div>
+                            </div>
+                              )
+                          })}
+                            
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="header-img">
+                            <div className="img-item">
+                                <img src="https://imgupload.io/images/2020/12/29/category-1.jpg" />
+                                <a className="img-text" href="">
+                                    <p></p>
+                                </a>
+                            </div>
+                            <div className="img-item">
+                                <img src="https://imgupload.io/images/2020/12/29/category-2.jpg" />
+                                <a className="img-text" href="">
+                                    <p></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="feature">
+            <div className="container-fluid">
+                <div className="row align-items-center">
+                    <div className="col-lg-4 col-md-12 feature-col">
+                        <div className="feature-content">
+                            <i className="fab fa-cc-mastercard"></i>
+                            <h2>100% sécurisé</h2>
+                            <p>
+                                Tous les transaction se passent d'une manière 100% sécurisée 
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 feature-col">
+                        <div className="feature-content">
+                            <i className="fa fa-truck"></i>
+                            <h2>Partout dans le monde</h2>
+                            <p>
+                                Nous livrons tous nos produits dans la plupart des pays du monde
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-12 feature-col">
+                        <div className="feature-content">
+                            <i className="fa fa-comments"></i>
+                            <h2>24/7 Support</h2>
+                            <p>
+                                Notre support est disponible 24h/24 7j/7
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="call-to-action">
+            <div className="container-fluid">
+                <div className="row align-items-center">
+                    <div className="col-md-6">
+                        <h1>Pour plus d'informations, appelez nous sur :</h1>
+                    </div>
+                    <div className="col-md-6">
+                        <a href="tel:+212500000000">+2125 00 00 00 00</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="newsletter">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h1>Subscribe Our Newsletter</h1>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="form">
+                            <input readOnly type="email" value="Your email here"/>
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="review">
+            <div className="container-fluid">
+                <div className="row align-items-center review-slider normal-slider">
+                    <div className="col-md-6">
+                        <div className="review-slider-item">
+                            <div className="review-img">
+                                <img src="https://imgupload.io/images/2020/12/29/review-1.jpg" alt="Image"/>
+                            </div>
+                            <div className="review-text">
+                                <h2>Morgana</h2>
+                                <h3>Professeur</h3>
+                                <p>
+                                    Les produits sont de bonnes qualité
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="review-slider-item">
+                            <div className="review-img">
+                                <img src="https://imgupload.io/images/2020/12/29/review-2.jpg" alt="Image"/>
+                            </div>
+                            <div className="review-text">
+                                <h2>Ezreal</h2>
+                                <h3>Banquier</h3>
+                                <p>
+                                    J'ai bien reçu ma commande dans un temps exceptionnel, je recommande bien cette application.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="review-slider-item">
+                            <div className="review-img">
+                                <img src="https://imgupload.io/images/2020/12/29/review-3.jpg" alt="Image"/>
+                            </div>
+                            <div className="review-text">
+                                <h2>Fiora</h2>
+                                <h3>Comptable</h3>
+                                <p>
+                                    Articles à la hauteur des ambitions, je recommande cette plateforme.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+  }
+}
+
+export default Accueil
---
+++ resources/js/App/Pages/Produits.js
@@ -0,0 +1,248 @@
+import React, { Component } from 'react'
+import {Link} from "react-router-dom"
+class Produits extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            products:[],selected:0,loaded:false,filter:false,cat:"",price:"",tailles:"",
+            categories:['homme','femme','adulte','enfant','chemise','pantalon','veste','menteau'],
+            search:props.sf
+
+        }
+    }
+    splitInPages(arr,n){
+        let res=[]
+        while(arr.length>0){
+            res.push(arr.splice(0,n))
+        }
+        return res
+    }
+    componentDidMount=()=>{
+        
+        if(this.state.search){
+            let p=this.props.pdisp()
+            console.log(p)
+            this.setState({products:this.splitInPages(p,12),loaded:true})
+        }
+        else{
+            fetch("/products")
+        .then(body=>body.json())
+        .then(obj=>{
+            this.setState({products:this.splitInPages(obj,12),loaded:true})}
+            )
+        }
+        
+    }
+    changePage=(e)=>{
+        let c=e.target.innerHTML
+        if(c=="Précédent"){
+            if(this.state.selected>=1)
+                this.setState({selected:this.state.selected-1})
+        }
+        else if(c=="Suivant"){
+            if(this.state.selected<=this.state.products.length-2)
+                this.setState({selected:this.state.selected+1})
+        }
+        else if(this.state.selected!=parseInt(c)-1) this.setState({selected:parseInt(c)-1})
+    }
+    handleTrans=(e)=>{
+        let i=e.target.getAttribute("data-index")
+        console.log(i)
+        console.log(this.state.products)
+        this.props.trans(this.state.products[this.state.selected][i])
+    }
+    HandleFilter=(type,value)=>{
+        if(type==="cat"){
+            var cats=document.querySelectorAll(".catFilter")
+            var str=""
+            for(let i=0; i<cats.length;i++){
+                let ch=cats[i].checked?this.state.categories[i]+"-":""
+                str=str+ch
+            }
+            if(str){
+                str=str.split('-')
+                str.splice(-1,1)
+                str=str.join("-")
+            }
+            this.setState({filter:true,cat:str})
+            setTimeout(()=>{this.fetchFilter()},300)
+            
+        }
+        else if(type==="taille" || type==="prix"){
+            console.log(document.querySelector("#"+value).value)
+            this.setState({[value]:document.querySelector("#"+value).value})
+            this.fetchFilter()
+        }
+    }
+    fetchFilter=()=>{
+        let minp=this.state.minprix>=0?this.state.minprix:0
+        let maxp=this.state.maxprix<=5000?this.state.maxprix:5000
+        console.log(this.state.cat)
+        if(this.state.cat){
+            console.log("54515")
+        fetch("/products/"+this.state.cat+"/"+minp+"-"+maxp+"/0-50/")
+            .then(body=>body.json())
+            .then(obj=>{
+                this.setState({products:this.splitInPages(obj,12),loaded:true})
+            }
+                
+                )
+        }
+        else{
+            fetch("/products/homme-femme-adulte-enfant-chemise-pantalon-veste-menteau/"+minp+"-"+maxp+"/0-50/")
+            .then(body=>body.json())
+            .then(obj=>{
+                this.setState({products:this.splitInPages(obj,12),loaded:true})}
+                )
+        }
+    }
+  render() {
+    return (
+        <div className="product-view">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-8">
+                <a className="btn btn-primary m-1" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    Filtres
+                </a>
+                <div className="collapse" id="collapseExample">
+                    <div className="card card-body m-1">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","1")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck1"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck1">
+                                        Homme
+                                    </label>
+                                </div>
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","2")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck2"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck2">
+                                        Femme
+                                    </label>
+                                </div>
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","3")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck3"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck3">
+                                        Adulte
+                                    </label>
+                                </div>
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","4")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck4"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck4">
+                                        Enfant
+                                    </label>
+                                </div>
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","5")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck5"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck5">
+                                        Chemise
+                                    </label>
+                                </div>
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","6")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck6"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck6">
+                                        Pantalon
+                                    </label>
+                                </div>
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","7")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck7"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck7">
+                                        Veste
+                                    </label>
+                                </div>
+                                <div className="form-check-inline">
+                                    <input onChange={()=>{this.HandleFilter("cat","8")}} className="form-check-input catFilter" type="checkbox" value="" id="defaultCheck8"/>
+                                    <label className="form-check-label" htmlFor="defaultCheck8">
+                                        Menteau
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="col-lg-3">
+                                <p>Prix</p>
+                                <input min="0" max="5000" id="minprix" onChange={()=>{this.HandleFilter("prix","minprix")}} type="number"/><span>  -   </span><input onChange={()=>{this.HandleFilter("prix","maxprix")}} id="maxprix" min="0" max="5000" type="number"/>
+                            </div>
+                            <div className="col-lg-3">
+                                <p>Tailles</p>
+                                <input onChange={()=>{this.HandleFilter("taille","mintaille")}} id="mintaille" min="0" max="50" type="number"/><span>  -   </span><input onChange={()=>{this.HandleFilter("taille","maxtaille")}} id="maxtaille" min="0" max="50" type="number"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    <div className="row">
+                        {
+                            this.state.loaded&&this.state.products.length>0?this.state.products[this.state.selected].map((elt,i)=>{
+                                return(
+                                <div key={i} className="col-md-4">
+                                    <div className="product-item">
+                                        <div className="product-title">
+                                            <a href="#">{elt.nom}</a>
+                                        </div>
+                                        <div className="product-image">
+                                            <img src={elt.img} alt="Product Image"/>
+                                        </div>
+                                        <div className="product-price">
+                                            <h3>{elt.prix_unitaire}<span>DH</span></h3>
+                                            <Link data-index={i} onClick={this.handleTrans} to="/details" className="btn" href="">Details</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}):this.state.products.length===0?"Rien à afficher":"loading..."
+                        }
+                    </div>
+                    <div className="col-md-12">
+                        <nav aria-label="Page navigation example">
+                            <ul className="pagination justify-content-center">
+                                <li className={this.state.selected===0?"page-item hidepg":"page-item"}>
+                                    <a onClick={this.changePage} className="page-link" href="#" tabIndex="-1">Précédent</a>
+                                </li>
+                                {this.state.products.map((e,i)=>
+                                    <li key={i} className={this.state.selected===i?"page-item active":"page-item"}><a onClick={this.changePage} className="page-link" href="#">{i+1}</a></li>
+                                )}
+                                <li className={this.state.selected===this.state.products.length-1?"page-item hidepg":"page-item"}>
+                                    <a onClick={this.changePage} className="page-link" href="#">Suivant</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>           
+
+                <div className="col-lg-4 sidebar">
+                    <div className="sidebar-widget category">
+                        <h2 className="title">Category</h2>
+                        <nav className="navbar bg-light">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#"><i className="fa fa-female"></i>Fashion et Beauté</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#"><i className="fa fa-tshirt"></i>Hommes et Femmes</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    
+                    <div className="sidebar-widget tag">
+                        <h2 className="title">Mots clés</h2>
+                        <a href="#">Lorem ipsum</a>
+                        <a href="#">Vivamus</a>
+                        <a href="#">Phasellus</a>
+                        <a href="#">pulvinar</a>
+                        <a href="#">Curabitur</a>
+                        <a href="#">Fusce</a>
+                        <a href="#">Sem quis</a>
+                        <a href="#">Mollis metus</a>
+                        <a href="#">Sit amet</a>
+                        <a href="#">Vel posuere</a>
+                        <a href="#">orci luctus</a>
+                        <a href="#">Nam lorem</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
+
+export default Produits
